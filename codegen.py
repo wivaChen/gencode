e058ab3e4953556ce226f833d1f4b2b87d3f30c9
wivaChen <chenyi3315@163.com>
2017-03-08 23:38:49 +0800

    Update codegen.py

--- codegen.py
+++ codegen.py
@@ -1,170 +1,38 @@
-#--*-- coding:utf8 --*--
-import os
-import sys
-import string
-import datetime
+from comm import *
+from head import *
+from cppfile import *
+from xmlfile import *
 
-g_map_type = {
-   'VARCHAR2':'SqliteData::DataType::String',
-   'NVARCHAR2':'SqliteData::DataType::String',
-   'NUMBER':'SqliteData::DataType::Int',
-   'DATE':'SqliteData::DataType::TimeStamp',
-   'BLOB':'SqliteData::DataType::ByteArray',
-}
+if __name__ == '__main__': 
+    classname = ''
+    structname = ''
 
-g_map_type_func = {
-   'VARCHAR2':'toString',
-   'NVARCHAR2':'toString',
-   'NUMBER':'toInt',
-   'DATE':'toString',
-   'BLOB':'toString',
-}
+    tableField.append(Field('TEST_ID', 'NUMBER', 9, 'Y', '', True))
+    tableField.append(Field('TEST_DESC', 'VARCHAR2', 64, 'N', '', False))
+    tableField.append(Field('TEST_FLAG', 'NUMBER', 12, 'N', '', False))
 
-g_map_nullable = {
-    'Y':'true',
-    'N':'false',
-}
-
-g_map_iskey = {
-    True:'true',
-    False:'false',
-}
- 
-class Field(object):
-    def __init__(self, name, type, len, nullable, comments='', iskey=False):
-        self.name = name
-        self.type = type
-        self.len = len
-        self.nullable = nullable
-        self.comments = comments
-        self.iskey = iskey
-        
-def gencomm(filename):
-#    if os.path.isfile(filename):
-#        print '%s already exist!' % filename
-#        os.remove(filename)
-#        print '%s removed!' % filename 
-    file = open(filename, 'w')
-    today = datetime.date.today()
-    date = today.strftime('%Y')+'-'+today.strftime('%m')+'-'+today.strftime('%d')
-    filetypes = string.split(filename,'.')
-    length = len(filetypes)
-    filetype = filetypes[length-1]
-     
-    if filetype == 'c' or filetype == 'cpp' or filetype == 'h':
-        file.writelines('/**')
-        file.write('\n')
-        file.writelines('* @File: '+ filename)
-        file.write('\n')
-        file.write('* @Date: '+date)
-        file.write('\n')
-        file.write('* @author: ')
-        file.write('\n')
-        file.write('*/\n')
-    else:
-        print 'just create %s' % filename
-    file.close()
-    
-def genFuncDef(file):
-    for i in range(len(tableField)):
-        fieldsplit = tableField[i].name.lower().split('_')
-        fieldname = ''
-        for j in range(len(fieldsplit)):
-            fieldname += fieldsplit[j].capitalize()
-        if tableField[i].type == 'NUMBER':
-            if tableField[i].len <= 9:
-                file.write('    void set' + fieldname + '(int32_t nValue);')
-            else:
-                file.write('    void set' + fieldname + '(int64_t lValue);')
-        elif tableField[i].type == 'VARCHAR2':
-            file.write('    void set' + fieldname + '(const char* szValue);')
-        else:
-            print 'unkown type'
-        file.write('\n')
-        
-    file.write('\n\n')
-    
-    for i in range(len(tableField)):
-        fieldsplit = tableField[i].name.lower().split('_')
-        fieldname = ''
-        for j in range(len(fieldsplit)):
-            fieldname += fieldsplit[j].capitalize()
-        if tableField[i].type == 'NUMBER':
-            if tableField[i].len <= 9:
-                file.write('    int32_t get' + fieldname + '();')
-            else:
-                file.write('    int64_t get' + fieldname + '();')
-        elif tableField[i].type == 'VARCHAR2':
-            file.write('    const char* get' + fieldname + '();')
-        else:
-            print 'unkown type'
-        file.write('\n')
-        
-def genheadfile(filename):
-    gencomm(filename)
-    file = open(filename,'a+')   
-    headmacro = '__' + filename.split('.')[0].upper() + '_H__'
-    file.writelines('#ifndef ' + headmacro)
-    file.write('\n')
-    file.writelines('#define ' + headmacro)
-    file.write('\n\n\n')
-    file.write('struct ' + structname)
-    file.write('\n{\n')
-    for i in range(len(tableField)):
-        fieldsplit = tableField[i].name.lower().split('_')
-        fieldname = ''
-        for j in range(len(fieldsplit)):
-            fieldname += fieldsplit[j].capitalize()   
-        if tableField[i].type == 'NUMBER':
-            if tableField[i].len <= 9:
-                file.write('    int32_t n' + fieldname + ';')
-            else:
-                file.write('    int64_t l' + fieldname + ';')
-        elif tableField[i].type == 'VARCHAR2':
-            file.write('    char sz' + fieldname + '[' + str(tableField[i].len) + '];')
-        else:
-            print 'unkown type'
-        file.write('\n')
-    file.write('};')
-    file.write('\n\n\n')
-    file.write('class ' + classname + ' : ' + 'public RBase<' + structname + '>')
-    file.write('\n{')
-    file.write('\npublic:\n')
-    genFuncDef(file)
-    file.write('\n};')
-    file.write('\n\n#endif\n')
-    file.close() 
-    
-def gencppfile(filename):
-    gencomm(filename)
-    file = open(filename,'a+')
-    includefile = '''#include "''' + headfile + '"'
-    file.write(includefile)
-    file.close() 
-    
-if __name__ == '__main__':    
-
-    table_name = 'CACHE_TEST_T'
-    tableField = []
-    tableField.append(Field('TEST_ID', 'NUMBER', 9, 'Y'))
-    tableField.append(Field('TEST_DESC', 'VARCHAR2', 64, 'N'))
-    tableField.append(Field('TEST_FLAG', 'NUMBER', 12, 'N'))
-    
+    print table_name
     tablesplit = table_name.lower().split('_')
     tablesplit.remove('t')
-    classname = 'R'
-    structname = 'T'
+    classname += 'C'
+    structname += 'T'
     for i in range(len(tablesplit)):
         classname += tablesplit[i].capitalize()
         structname += tablesplit[i].capitalize()
-    print 'classname = %s' % classname
 
+    print classname
+    print structname
+    
     headfile = classname + '.h'
     cppfile = classname + '.cpp'
-
+    xmlfile = table_name + '.bo.xml'
+    
     print 'generate head file: %s' % headfile
-    genheadfile(headfile)
+    genheadfile(headfile, classname, structname)
     
     print 'generate cpp file: %s' % cppfile
-    gencppfile(cppfile)
+    gencppfile(cppfile, headfile)
+    
+    print 'generate xml file: %s' % xmlfile
+    genxmlfile(xmlfile,table_name)
     

a9d861367e414bbc979647fbd0d1e622967761e2
wivaChen <chenyi3315@163.com>
2017-03-12 22:01:45 +0800

    Update codegen.py

--- codegen.py
+++ codegen.py
@@ -7,9 +7,11 @@ if __name__ == '__main__':
     classname = ''
     structname = ''
 
-    tableField.append(Field('TEST_ID', 'NUMBER', 9, 'Y', '', True))
-    tableField.append(Field('TEST_DESC', 'VARCHAR2', 64, 'N', '', False))
-    tableField.append(Field('TEST_FLAG', 'NUMBER', 12, 'N', '', False))
+    tableField.append(Field('TEST_ID', 'NUMBER', 9, 'N', '', True))
+    tableField.append(Field('TEST_SEQ', 'VARCHAR2', 3, 'N', '', True))
+    tableField.append(Field('TEST_DESC', 'VARCHAR2', 64, 'Y', '', False))
+    tableField.append(Field('TEST_FLAG', 'NUMBER', 12, 'Y', '', False))
+    tableField.append(Field('TEST_DATE', 'DATE' , 12, 'Y', '', False))
 
     print table_name
     tablesplit = table_name.lower().split('_')
@@ -31,7 +33,7 @@ if __name__ == '__main__':
     genheadfile(headfile, classname, structname)
     
     print 'generate cpp file: %s' % cppfile
-    gencppfile(cppfile, headfile)
+    gencppfile(cppfile, headfile, classname, structname)
     
     print 'generate xml file: %s' % xmlfile
     genxmlfile(xmlfile,table_name)
